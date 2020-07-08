d7e155f05fa869b3974b6919c33ea8b09c8b850f
Raul Sposito <raulsposito@gmail.com>
2020-07-08 04:04:36 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,11 @@
 class Pokemon
+
+  attr_reader :id 
+  attr_accessor :name, :type, :db
+
+  def initialize(id = nil, name, type, db)
+    @id = id 
+    @name = name
+    @type = type 
+    @db = db 
 end
