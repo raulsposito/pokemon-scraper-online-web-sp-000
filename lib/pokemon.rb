7cda03ccc2eab8ec0813fa51cf47e80e5c3fe359
Raul Sposito <raulsposito@gmail.com>
2020-07-08 04:07:11 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,11 +3,8 @@ class Pokemon
   attr_reader :id
   attr_accessor :name, :type, :db
 
-  def initialize(id = nil, name, type, db)
-    @id = id
-    @name = name
-    @type = type
-    @db = db
+  def initialize(id:, name:, type:, hp: nil, db:)
+    @id, @game, @type, @hp, @db = id, name, type, hp, db
   end
 
 end
